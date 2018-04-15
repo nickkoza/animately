fe6edf367de0d1164d4ae8037d65e55a149d707a
Nicholas Koza <NicholasKoza@gmail.com>
2018-04-15 16:27:17 -0700

    Adding tweens and examples

--- examples/LEDs/LEDs.cpp
+++ examples/LEDs/LEDs.cpp
@@ -2,73 +2,229 @@
 // This #define increases the number of animations that we can schedule at one time.
 #define TIMELINE_MAX_SCHEDULED_ENTRIES 20
 
-
 #include "Animately.h"
 #include "Core/Timeline.h"
 #include "Parts/LED.h"
-#include "Tweens/Flicker.h"
+#include "Tweens/Quadratic.h"
+#include "Tweens/Quintic.h"
+#include "Tweens/Exponential.h"
+#include "Tweens/Sine.h"
+#include "Tweens/BounceOut.h"
 
 
 // By defining that we're using the Animately namespace, we don't have to prefix all Animately classes with "Animately::"
 using namespace Animately;
 
 
-// We could schedule animations in the setup() function, but by putting them into a class like Scene
+// We could schedule animations in the setup() function, but by putting them into scene classes like this
 // we can chain them together so an animation ending schedules the start of another animation.
-class Scene {
+
+// The chase animation triggers the LEDs one after the other, with a smoothed overlap using quadratic easing
+class ChaseScene {
 private:
     Timeline *timeline;
-    LED *led1;
-    LED *led2;
-    LED *led3;
-    LED *led4;
-    Tween *sparkTween;
+    LED *leds[4];
+    Quadratic *tween;
 
 public:
-    Scene(Timeline *timeline, LED *led1, LED *led2, LED *led3, LED *led4, Tween *sparkTween) {
+    ChaseScene(Timeline *timeline, LED *leds[], Quadratic *tween) {
         this->timeline = timeline;
-        this->led1 = led1;
-        this->led1 = led1;
-        this->led1 = led1;
-        this->led1 = led1;
-        this->sparkTween = sparkTween;
+        memcpy(this->leds, leds, sizeof(LED*) * 4);
+        this->tween = tween;
     }
     
-    void cueChase() {
-        //timeline.schedule();
-    }
+    // The chase animation triggers the LEDs one after the other, with a smoothed overlap using quadratic easing
+    void cue(int val) {
+        
+        for (int i = 0; i < 4; i++) {
+            int offset = i * 500;
+            timeline->schedule(0, 255, offset, 200,
+                NULL,
+                DELEGATE(leds[i], &LED::set),
+                DELEGATE(tween, &Quadratic::inOut),
+                DELEGATE(leds[i], &LED::set));    
 
-    void cueFade() {
-        //timeline.schedule();
-    }
+            timeline->schedule(255, 0, offset + 300, 200,
+                NULL,
+                DELEGATE(leds[i], &LED::set),
+                DELEGATE(tween, &Quadratic::inOut),
+                DELEGATE(leds[i], &LED::off));
+        }
 
-    void cueBounce() {
-        //timeline.schedule();
+        // Schedule it to play again
+        timeline->schedule(2000, 0, NULL, DELEGATE(this, &ChaseScene::cue));
+    }
+};
+
+
+// The fade animation slowly fades all 4 LEDs in and out, with 4 different tweens/easings
+class FadeScene {
+private:
+    Timeline *timeline;
+    LED *leds[4];
+    Quadratic *quadratic;
+    Exponential *exponential;
+    Quintic *quintic;
+    Sine *sine;
+
+public:
+    FadeScene(Timeline *timeline, LED *leds[], Quadratic *quadratic, Exponential *exponential, Quintic *quintic, Sine *sine) {
+        this->timeline = timeline;
+        memcpy(this->leds, leds, sizeof(LED*) * 4);
+        this->quadratic = quadratic;
+        this->exponential = exponential;
+        this->quintic = quintic;
+        this->sine = sine;
+    }        
+
+    void cue(int val) {
+        timeline->schedule(0, 255, 0, 1000,
+            NULL,
+            DELEGATE(leds[0], &LED::set),
+            DELEGATE(quadratic, &Quadratic::in),
+            DELEGATE(leds[0], &LED::set));
+
+        timeline->schedule(255, 0, 1000, 1000,
+            NULL,
+            DELEGATE(leds[0], &LED::set),
+            DELEGATE(quadratic, &Quadratic::out),
+            DELEGATE(leds[0], &LED::off));
+        
+            
+        timeline->schedule(0, 255, 0, 1000,
+            NULL,
+            DELEGATE(leds[1], &LED::set),
+            DELEGATE(exponential, &Exponential::in),
+            DELEGATE(leds[1], &LED::set));
+
+        timeline->schedule(255, 0, 1000, 1000,
+            NULL,
+            DELEGATE(leds[1], &LED::set),
+            DELEGATE(exponential, &Exponential::out),
+            DELEGATE(leds[1], &LED::off));
+
+ 
+        timeline->schedule(0, 255, 0, 1000,
+            NULL,
+            DELEGATE(leds[2], &LED::set),
+            DELEGATE(quintic, &Quintic::in),
+            DELEGATE(leds[2], &LED::set));
+
+        timeline->schedule(255, 0, 1000, 1000,
+            NULL,
+            DELEGATE(leds[2], &LED::set),
+            DELEGATE(quintic, &Quintic::out),
+            DELEGATE(leds[2], &LED::off));
+
+
+        timeline->schedule(0, 255, 0, 1000,
+            NULL,
+            DELEGATE(leds[3], &LED::set),
+            DELEGATE(sine, &Sine::in),
+            DELEGATE(leds[3], &LED::set));
+
+        timeline->schedule(255, 0, 1000, 1000,
+            NULL,
+            DELEGATE(leds[3], &LED::set),
+            DELEGATE(sine, &Sine::out),
+            DELEGATE(leds[3], &LED::off));
+
+        // Schedule it to play again
+        timeline->schedule(2000, 0, NULL, DELEGATE(this, &FadeScene::cue));
+    }
+};
+
+
+// The bounce animation runs a basic chase but "bounces" at the end
+class BounceScene {
+private:
+    Timeline *timeline;
+    LED *leds[4];
+    Exponential *tween;
+    BounceOut *bounceTween;
+    
+public:
+    BounceScene(Timeline *timeline, LED *leds[], Exponential *tween, BounceOut *bounceTween) {
+        this->timeline = timeline;
+        memcpy(this->leds, leds, sizeof(LED*) * 4);
+        this->tween = tween;
+        this->bounceTween = bounceTween;
+    }
+    
+    void cue(int val) {
+        timeline->schedule(0, 255, 0, 200,
+            NULL,
+            DELEGATE(leds[0], &LED::set),
+            DELEGATE(tween, &Exponential::in),
+            DELEGATE(leds[0], &LED::off));
+
+        timeline->schedule(0, 255, 200, 200,
+            NULL,
+            DELEGATE(leds[1], &LED::set),
+            DELEGATE(tween, &Exponential::in),
+            DELEGATE(leds[1], &LED::off));
+
+        timeline->schedule(0, 255, 400, 200,
+            NULL,
+            DELEGATE(leds[2], &LED::set),
+            DELEGATE(tween, &Exponential::in),
+            DELEGATE(leds[2], &LED::off));
+
+        timeline->schedule(0, 255, 600, 100,
+            NULL,
+            DELEGATE(leds[3], &LED::set),
+            DELEGATE(tween, &Exponential::in),
+            DELEGATE(leds[3], &LED::set));
+
+        timeline->schedule(255, 0, 700, 600,
+            NULL,
+            DELEGATE(leds[3], &LED::set),
+            DELEGATE(bounceTween, &BounceOut::getValue),
+            DELEGATE(leds[3], &LED::off));
+        
+        // Schedule it to play again
+        timeline->schedule(2000, 0, NULL, DELEGATE(this, &BounceScene::cue));
     }
 };
 
 
 // Setup all our parts
 Timeline timeline;
-LED led1(9);
-LED led2(10);
-LED led3(11);
-LED led4(12);
+LED led1(6);
+LED led2(9);
+LED led3(10);
+LED led4(11);
+
+LED *leds[] = {
+    &led1,
+    &led2,
+    &led3,
+    &led4
+};
 
 
 // Setup all our tweens
-Flicker sparkTween({0, 255}, {15, 50}, {2, 10}, {15, 50});
+Quadratic quadratic;
+Quintic quintic;
+Exponential exponential;
+Sine sine;
+BounceOut bounceOut;
 
 
-// Create the scene
-Scene scene(&timeline, &led1, &led2, &led3, &led4, &sparkTween);
+// Create the scenes
+ChaseScene chaseScene(&timeline, leds, &quadratic);
+FadeScene fadeScene(&timeline, leds, &quadratic, &exponential, &quintic, &sine);
+BounceScene bounceScene(&timeline, leds, &exponential, &bounceOut);
+
 
 
 void setup() {
     Serial.begin(9600);
     
     // Start the scene
-    scene.cueChase();
+    //chaseScene.cue(0);
+    //bounceScene.cue(0);
+    fadeScene.cue(0);
 }
 
 
---
+++ src/Tweens/Sine.h
@@ -0,0 +1,52 @@
+// Based on Robert Penner's easing functions: http://robertpenner.com/easing/
+// and adapted from Warren Moore's C conversion: https://github.com/warrenm/AHEasing
+// Easing algorithms can be visualized on http://easings.net/
+//
+// Robert Penner's easing functions are open-sourced under the BSD License:
+// TERMS OF USE - EASING EQUATIONS
+//
+// Open source under the BSD License.
+//
+// Copyright © 2001 Robert Penner
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+// following conditions are met:
+//
+// Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+// disclaimer.
+// Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided with the distribution.
+// Neither the name of the author nor the names of contributors may be used to endorse or promote products derived
+// from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+#ifndef SINE_H_
+#define SINE_H_
+
+namespace Animately {
+    class Sine {
+        public:
+        float in(float p) {
+            return sin((p - 1) * M_PI_2) + 1;
+        }
+        
+        float out(float p) {
+            return 0.5 * (1 - cos(p * M_PI));
+        }
+        
+        float inOut(float p) {
+            return sin(p * M_PI_2);
+        }
+    };
+}
+
+#endif /* SINE_H_ */
