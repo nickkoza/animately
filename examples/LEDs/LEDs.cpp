9d762fa0446f5280de6ba55d884c1b41179ff4d4
Nicholas Koza <NicholasKoza@gmail.com>
2018-04-14 23:23:12 -0700

    Prepping for Arduino library deployment

---
+++ examples/LEDs/LEDs.cpp
@@ -0,0 +1,50 @@
+// Configuration for Animately must happen BEFORE including Animately.h.
+#define TIMELINE_MAX_SCHEDULED_ENTRIES 20
+
+#include "Animately.h"
+#include "Core/Timeline.h"
+#include "Parts/LED.h"
+#include "Tweens/Flicker.h"
+
+using namespace Animately;
+
+Timeline timeline;
+
+class Scene {
+private:
+    Timeline timeline;
+    LED led1(9);
+    LED led2(10);
+    LED led3(11);
+    LED led4(12);
+    Flicker sparkTween({0, 255}, {15, 50}, {2, 10}, {15, 50});
+
+public:
+    
+    void cueChase() {
+        //timeline.schedule();
+    }
+
+    void cueFade() {
+        //timeline.schedule();
+    }
+
+    void cueBounce() {
+        //timeline.schedule();
+    }
+
+    void tick() {
+        timeline.tick();
+    }
+}
+
+Scene scene;
+
+void setup() {
+    Serial.begin(9600);
+    scene.cueChase();
+}
+
+void loop() {
+    scene.tick();
+}
